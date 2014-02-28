1b7b195cd043b5bfc4148437e5fa5ea797e89a3d
waTeim <truthset@gmail.com>
2014-02-27 22:02:38 -0500

    Added include, lib directories.  Modified podspec to reflect that.

---
+++ include/httpserver.hpp
@@ -0,0 +1,16 @@
+#ifndef _HTTPSERVER_HPP_
+#define _HTTPSERVER_HPP_
+
+#define _HTTPSERVER_HPP_INSIDE_
+
+#include "httpserver/http_utils.hpp"
+#include "httpserver/details/http_endpoint.hpp"
+#include "httpserver/http_resource.hpp"
+#include "httpserver/http_response.hpp"
+#include "httpserver/http_request.hpp"
+#include "httpserver/event_supplier.hpp"
+#include "httpserver/details/event_tuple.hpp"
+#include "httpserver/details/http_resource_mirror.hpp"
+#include "httpserver/webserver.hpp"
+
+#endif
